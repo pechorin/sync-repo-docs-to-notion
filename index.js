cbdc2ed36345b63289ec442797e96334964d95ea
andrey pechorin <pechorin.andrey@gmail.com>
2023-06-27 16:23:17 +0300

    lint

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
-const fs = require("fs");
-const { globSync } = require("glob");
-const { markdownToBlocks } = require('@tryfabric/martian');
-const { Client } = require("@notionhq/client");
-const { nextTick } = require("process");
-const { root } = require("@tryfabric/martian/build/src/markdown");
+const fs = require('fs')
+const { globSync } = require('glob')
+const { markdownToBlocks } = require('@tryfabric/martian')
+const { Client } = require('@notionhq/client');
+// const { execSync } = require('child_process');
 
 // TODO: add sleep interval for all requests
 // TODO: fix tables width
@@ -18,33 +17,33 @@ const { root } = require("@tryfabric/martian/build/src/markdown");
 
 const notionUrlMatch = process.env.NOTION_ROOT_ID.match(/[^-]*$/)
 if (notionUrlMatch == null) {
-  throw new SyntaxError('Provided page was not in a valid format, url must end with "-<page-id>"');
+  throw new SyntaxError('Provided page was not in a valid format, url must end with "-<page-id>"')
 }
 const notionPageId = notionUrlMatch[0]
 
 const notion = new Client({
-  auth: process.env.NOTION_TOKEN 
+  auth: process.env.NOTION_TOKEN
 })
 
 notion.pages.retrieve({ page_id: notionPageId }).then((rootPage) => {
   // console.log("Root page-> ", rootPage)
 
-  var files = globSync(`${process.env.FOLDER}/**/*.md`, { ignore: 'node_modules/**' })
+  const files = globSync(`${process.env.FOLDER}/**/*.md`, { ignore: 'node_modules/**' })
   // console.log("Files to sync ->", files)
 
-  notion.blocks.children.list({block_id: notionPageId}).then((blocksResponse) => {
+  notion.blocks.children.list({ block_id: notionPageId }).then((blocksResponse) => {
     const blockIdsToRemove = blocksResponse.results.map((e) => e.id)
 
     // sequencially delete all page blocks
-    var doDelete = function(idToDelete) {
-      if (!idToDelete) return;
+    const doDelete = function (idToDelete) {
+      if (!idToDelete) return
 
       notion.blocks.delete({
         block_id: idToDelete
-      }).then(function() {
-        console.log("Block deleted:", idToDelete)
-        if (idToDelete != blockIdsToRemove[blockIdsToRemove.length - 1]) {
-          nextDeleteId = blockIdsToRemove[blockIdsToRemove.indexOf(idToDelete) + 1]
+      }).then(function () {
+        console.log('Block deleted:', idToDelete)
+        if (idToDelete !== blockIdsToRemove[blockIdsToRemove.length - 1]) {
+          const nextDeleteId = blockIdsToRemove[blockIdsToRemove.indexOf(idToDelete) + 1]
           doDelete(nextDeleteId)
         }
       })
@@ -53,49 +52,47 @@ notion.pages.retrieve({ page_id: notionPageId }).then((rootPage) => {
     doDelete(blockIdsToRemove[0])
     console.log('Block deletion complete')
 
-    var doCreate = (filePath) => {
+    const doCreate = (filePath) => {
       const mdContent = fs.readFileSync(filePath, 'utf8')
       const newBlocks = markdownToBlocks(mdContent)
 
-      var title
+      let title
 
       try {
         title = newBlocks[0][newBlocks[0].type].rich_text[0].text.content
       } catch (error) {
-        console.log("Cannot extract page title from", newBlocks[0])
+        console.log('Cannot extract page title from', newBlocks[0])
         process.exit(1)
       }
 
       notion.pages.create({
         parent: {
-          type: "page_id",
+          type: 'page_id',
           page_id: rootPage.id
         },
         properties: {
-          "title": {
-            "title": [{ "text": { "content": title} }], type: "title"
+          title: {
+            title: [{ text: { content: title } }], type: 'title'
           }
-        },
+        }
       }).then((pageResponse) => {
         console.log('Page created', pageResponse)
 
         notion.blocks.children.append({ block_id: pageResponse.id, children: newBlocks }).then(() => {
           // process next page
-          if (filePath != files[files.length - 1]) {
+          if (filePath !== files[files.length - 1]) {
             doCreate(files[files.indexOf(filePath) + 1])
           }
         })
-
       }).catch((error) => {
-        console.log("Page creation failed", error)
+        console.log('Page creation failed', error)
         process.exit(1)
       })
     }
 
     doCreate(files[0])
   })
-
 }).catch((error) => {
-  console.log("Root page not found", error.body)
+  console.log('Root page not found', error.body)
   process.exit(1)
 })
